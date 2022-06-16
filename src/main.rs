6250f8afd27a661fe365fd6a02384ed46ae1072f
CelesteDev <dellangesophie@gmail.com>
2022-06-16 08:53:26 +0200

    Added App and Camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use bevy::prelude::*;
+
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+
+use bevy_inspector_egui::{
+    Inspectable, RegisterInspectable, WorldInspectorParams, WorldInspectorPlugin,
+};
+
+const WINDOW_HEIGHT: f32 = 720.;
+const WINDOW_RESOLUTION: f32 = 16./9.;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: WINDOW_HEIGHT * WINDOW_RESOLUTION,
+            height: WINDOW_HEIGHT,
+            title: "jam 2022".to_string(),
+            resizable: false,
+            ..Default::default()
+        });
+}
+
+//this spawns a camera with a bottom to top Y coord system and a left to right X coord system
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+
+    camera.orthographic_projection.left = 0.;
+    camera.orthographic_projection.right = WINDOW_HEIGHT * WINDOW_RESOLUTION;
+
+    camera.orthographic_projection.top = WINDOW_HEIGHT;
+    camera.orthographic_projection.bottom = 0.;
+
+    commands.spawn_bundle(camera);
 }
