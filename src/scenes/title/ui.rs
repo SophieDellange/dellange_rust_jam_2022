3052c15f04ece5e35eff03357fd386cec9a865bb
giulio-Joshi <me@joshi.it>
2022-06-17 22:24:10 +0200

    Added wasm32 target compatibility
    Small test with wasm setup and compilation target.
    
    See WASM.md for instructions

--- src/scenes/title/ui.rs
+++ src/scenes/title/ui.rs
@@ -5,6 +5,19 @@ use crate::{
     ui::{Action, ActionMarker, EmbossedText, Housing, Overlay, SimpleText},
 };
 
+
+fn main_menu_texts(font: &Handle<Font>) -> (EmbossedText, SimpleText, SimpleText) {
+    (
+        EmbossedText::big("Rusty Jam\n\nDellange", font),
+        SimpleText::small(
+            "Sophie Dellange ÷ 64kramsystem ÷ Mechanought ÷ Joshi Spawnbrood",
+            font,
+        ),
+        SimpleText::small("\nTemplate by septum | https://septum.io", font),
+    )
+}
+
+
 #[derive(Component)]
 pub struct ScopedMarker;
 
@@ -14,6 +27,11 @@ fn spawn_camera(commands: &mut Commands) {
         .insert(ScopedMarker);
 }
 
+///
+/// The main title ui has two different options, depending on which target.
+///  this one is the main menu for desktop game.
+
+#[cfg(not(target_arch = "wasm32"))]
 pub fn spawn(commands: &mut Commands, fonts: &Fonts) {
     let font = &fonts.dogica;
     let button_size = Size::new(Val::Px(400.0), Val::Px(45.0));
@@ -22,12 +40,11 @@ pub fn spawn(commands: &mut Commands, fonts: &Fonts) {
     let top = Housing::percent(100.0, 50.0);
     let bottom = Housing::percent(100.0, 50.0);
     let mut actions = Housing::percent(100.0, 90.0);
-    let authors_space = Housing::percent( 100.0, 15.0);
+    let authors_space = Housing::percent(100.0, 15.0);
     let footer = Housing::percent(100.0, 20.0);
 
-    let title = EmbossedText::big("Rusty Jam\n\nDellange", font);
-    let authors = SimpleText::small("Sophie Dellange ÷ 64kramsystem ÷ Mechanought ÷ Joshi Spawnbrood", font);
-    let notice = SimpleText::small("\nTemplate by septum | https://septum.io", font);
+    let (title, authors, notice) = main_menu_texts(font);
+
     let play = Action::new("Play", font, button_size);
     let quit = Action::new("Quit", font, button_size);
 
@@ -46,12 +63,58 @@ pub fn spawn(commands: &mut Commands, fonts: &Fonts) {
                     play.spawn(parent, ActionMarker::play());
                     quit.spawn(parent, ActionMarker::quit());
                 });
-                authors_space.spawn( parent, |parent| {
+                authors_space.spawn(parent, |parent| {
                     authors.spawn(parent);
                 });
 
                 footer.spawn(parent, |parent| {
-                    
+                    notice.spawn(parent);
+                });
+            });
+        },
+        ScopedMarker,
+    );
+
+    spawn_camera(commands);
+}
+
+/// This other one is the main menu for wasm, where buttons like "Quit" has no sense
+
+#[cfg(target_arch = "wasm32")]
+pub fn spawn(commands: &mut Commands, fonts: &Fonts) {
+    let font = &fonts.dogica;
+    let button_size = Size::new(Val::Px(400.0), Val::Px(45.0));
+
+    let overlay = Overlay::new();
+    let top = Housing::percent(100.0, 50.0);
+    let bottom = Housing::percent(100.0, 50.0);
+    let mut actions = Housing::percent(100.0, 90.0);
+    let authors_space = Housing::percent(100.0, 15.0);
+    let footer = Housing::percent(100.0, 20.0);
+
+    let (title, authors, notice) = main_menu_texts(font);
+
+    actions
+        .justify_content(JustifyContent::SpaceEvenly)
+        .align_items(AlignItems::Center);
+
+    let play = Action::new("Play", font, button_size);
+
+    overlay.spawn(
+        commands,
+        |parent| {
+            top.spawn(parent, |parent| {
+                title.spawn(parent);
+            });
+            bottom.spawn(parent, |parent| {
+                actions.spawn(parent, |parent| {
+                    play.spawn(parent, ActionMarker::play());
+                });
+                authors_space.spawn(parent, |parent| {
+                    authors.spawn(parent);
+                });
+
+                footer.spawn(parent, |parent| {
                     notice.spawn(parent);
                 });
             });
