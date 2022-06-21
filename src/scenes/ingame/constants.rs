3c52680db2fd8c6f64e3b01989a7f1b5a2518348
Saverio Miroddi <saverio.pub2@gmail.com>
2022-06-21 20:34:51 +0200

    Reduce preexisting loot count per level

--- src/scenes/ingame/constants.rs
+++ src/scenes/ingame/constants.rs
@@ -1,7 +1,7 @@
 pub const MAP_SIZE: (u16, u16) = (32, 15); // (width, height)
 
 pub const ENEMIES_COUNT: u8 = 16;
-pub const LOOT_COUNT: u8 = 16;
+pub const LOOT_COUNT: u8 = 4;
 
 pub const PLAYER_MOVE_SPEED: f32 = 7.5;
 pub const PET_MOVE_SPEED: f32 = 20.0;
